ba18a3cec46ae1266484dd05307c87dca549e164
maysha29 <mayshatabassum29@gmail.com>
2025-05-17 02:41:29 +0600

    Mongodb is connected

--- coffee-store-server/coffee-store-server/index.js
+++ coffee-store-server/coffee-store-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors=require('cors');
+require('dotenv').config()
 const app=express();
 const port=process.env.PORT || 5000;
 
@@ -8,12 +9,8 @@ const port=process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// hBIyyuCKGHB9Qexo
 
-
-const uri = `mongodb+srv://coffee_monster:hBIyyuCKGHB9Qexo@cluster0.esueazz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.esueazz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esueazz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
